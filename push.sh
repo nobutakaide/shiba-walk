ccade94effa2a3f5bbb20460f7c555460665b600
nobutakaide <ide.nobutaka@gmail.com>
2026-08-19 10:15:37 +0900

    アップ

--- push.sh
+++ push.sh
@@ -11,4 +11,8 @@ git commit -m "$msg"
 git push
 echo ""
 echo "処理が完了しました。"
+echo ""
+echo "リポジトリ: https://github.com/nobutakaide/shiba-walk"
+echo "公開ページ: https://nobutakaide.github.io/shiba-walk/"
+echo ""
 read -p "Enterキーを押すと閉じます..."
